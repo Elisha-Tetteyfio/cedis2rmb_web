e81bf68cb715a8f0b051870afe426c729d2eaf2c
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2024-09-09 07:35:18 +0000

    Add qr image

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,7 +40,27 @@ class OrdersController < ApplicationController
 
   # POST /orders or /orders.json
   def create
-    session[:order_params] = order_params
+    uploaded_image = order_params["recipient_account_attributes"]["qr_code"]
+
+    if uploaded_image.present?
+      blob = ActiveStorage::Blob.create_and_upload!(
+        io: uploaded_image.tempfile,
+        filename: uploaded_image.original_filename,
+        content_type: uploaded_image.content_type
+      )
+
+      # Store the blob id in the session
+      session["uploaded_image_blob_id"] = blob.id
+    end
+    # session[:order_params] = order_params
+    params_hash = order_params.to_h
+
+    # Remove qr_code from recipient_account_attributes
+    params_hash['recipient_account_attributes'].delete('qr_code')
+
+    # Convert hash back to ActionController::Parameters and store in session
+    session[:order_params] = ActionController::Parameters.new(params_hash)
+
     redirect_to summary_orders_path
   end
 
@@ -58,15 +78,18 @@ class OrdersController < ApplicationController
     @order = Order.new(order_params)
     @recipient_account = RecipientAccount.new(order_params["recipient_account_attributes"])
     @payer_account = PayerAccount.new(order_params["payer_account_attributes"])
-    @order.rate = ExchangeRate.current_rate.value
-    @order.status = "Pending"
-    @order.recipient_account = @recipient_account
-    @order.payer_account = @payer_account
-    @order.user = current_user if current_user
+
+    # Set qr_image
+    if session["uploaded_image_blob_id"]
+      qr = ActiveStorage::Blob.find(session["uploaded_image_blob_id"])
+      @recipient_account.qr_code.attach(qr)
+    end
+
+    set_order_properties
 
     respond_to do |format|
       if @order.save
-        session.delete(:order_params)
+        delete_session_data
         format.html { redirect_to "/admin_account/#{@order.id}", notice: "Order was successfully created." }
         format.json { render :show, status: :created, location: @order }
       else
@@ -110,7 +133,20 @@ class OrdersController < ApplicationController
       params.require(:order).permit(
         :amount, :user_id, :rate, :status, :whatsapp_number,
         payer_account_attributes: [:account_number, :account_name, :account_type_id],
-        recipient_account_attributes: [:account_number, :account_name, :account_type_id]
+        recipient_account_attributes: [:account_number, :account_name, :account_type_id, :qr_code]
       )
+    end
+
+    def set_order_properties
+      @order.rate = ExchangeRate.current_rate.value
+      @order.status = "Pending"
+      @order.recipient_account = @recipient_account
+      @order.payer_account = @payer_account
+      @order.user = current_user if current_user
+    end
+
+    def delete_session_data
+      session.delete(:order_params)
+      session.delete(:uploaded_image_blob_id)
     end
 end
