bc54958dd0f987dc8f22f9f25d735407bbadc4cb
Elisha Tetteyfio <tetteyfioelisha@gmail.com>
2024-10-20 08:18:52 +0000

    Update firestore.rb

--- config/initializers/firestore.rb
+++ config/initializers/firestore.rb
@@ -2,7 +2,7 @@ require "google/cloud/firestore"
 
 Google::Cloud::Firestore.configure do |config|
   config.project_id = "cedis2rmb"
-  config.credentials = "./config/cedis2rmb-1ccb40dc84cb.json"
+  config.credentials = JSON.parse(ENV['FIREBASE_CREDENTIALS'])
 end
 
 $firestore = Google::Cloud::Firestore.new
